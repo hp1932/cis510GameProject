a0cefdd584e2cdef808997d7948e8a893106ad7a
solcjoye <scj@uoregon.edu>
2016-04-28 16:38:49 -0700

    Most likely as much as I'll get done for Fri PoC presentation
    Link to video you can use David if you want, or you can load my portion
    into Unity, either way.

---
+++ 2D Demand Curve Drawing - Group Project/Assets/VertLineRender.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VertLineRender : MonoBehaviour {
+
+	private LineRenderer HorizontalLR;
+
+	public Transform LRplacement;
+	public float lineWidth;
+
+	// Use this for initialization
+	void Start () {
+
+		HorizontalLR = GetComponent<LineRenderer> ();
+		HorizontalLR.enabled = true;
+		HorizontalLR.useWorldSpace = true;
+		HorizontalLR.SetWidth(lineWidth, lineWidth);
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		RaycastHit2D hit = Physics2D.Raycast (transform.position, transform.up * -1);
+		// Debug.DrawLine (transform.position, hit.point, Color.red);
+		LRplacement.position = hit.point;
+		HorizontalLR.SetPosition (0, transform.position);
+		HorizontalLR.SetPosition (1, LRplacement.position);
+
+	}
+}
