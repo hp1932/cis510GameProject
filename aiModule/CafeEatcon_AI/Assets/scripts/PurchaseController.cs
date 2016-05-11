3750a2f708a6192c6e7a482f910f65118c601c34
bilbaeno <bilbaeno@uoregon.edu>
2016-05-10 21:48:21 -0700

    UI changes to phase2 and 3;

---
+++ aiModule/CafeEatcon_AI/Assets/scripts/PurchaseController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class PurchaseController : MonoBehaviour {
+
+	public RestaurantStatistics localPlayerData;
+
+	public Text breadCount_Text;
+	public Text turkeyCount_Text;
+	public Text hamCount_Text;
+	public Text veggieCount_Text;
+
+	
+	void Start () {
+
+		localPlayerData = GlobalControl.Instance.savedPlayerData;
+
+		breadCount_Text.text = localPlayerData.ingredientsOnHand ["Bread"].ToString();
+		turkeyCount_Text.text = localPlayerData.ingredientsOnHand ["Turkey"].ToString();
+		hamCount_Text.text = localPlayerData.ingredientsOnHand ["Ham"].ToString();
+		veggieCount_Text.text = localPlayerData.ingredientsOnHand ["Veggie"].ToString();
+
+	}
+
+}
