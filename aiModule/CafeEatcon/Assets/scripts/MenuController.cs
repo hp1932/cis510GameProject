680321ede21f8bc499ccb09f8c2b7de71c010c64
hannahp <hpruse@128-223-223-104-wireless.uoregon.edu>
2016-05-18 13:28:26 -0700

    Issue #34 - Added basic mute button
    Note that because of Issue #30, this only works if you start in phase 2
    to get the sound running first. This may need a re-skin to match the UI
    style of everything else.

---
+++ aiModule/CafeEatcon/Assets/scripts/MenuController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+	public static MenuController instance = null;
+
+
+	public void ToggleAudio()
+	{
+		SoundManager.instance.soundPaused = !SoundManager.instance.soundPaused;
+		AudioListener.pause = SoundManager.instance.soundPaused;
+	}
+}
--- aiModule/CafeEatcon/Assets/scripts/SoundManager.cs
+++ aiModule/CafeEatcon/Assets/scripts/SoundManager.cs
@@ -8,7 +8,7 @@ public class SoundManager : MonoBehaviour
 	public AudioSource efxSource;                   //Drag a reference to the audio source which will play the sound effects.
 	public AudioSource musicSource;                 //Drag a reference to the audio source which will play the music.
 	public static SoundManager instance = null;     //Allows other scripts to call functions from SoundManager.				
-
+	public bool soundPaused = false;
 
 	void Awake()
 	{
@@ -29,11 +29,14 @@ public class SoundManager : MonoBehaviour
 	//Used to play single sound clips.
 	public void PlaySingle(AudioClip clip)
 	{
-		//Set the clip of our efxSource audio source to the clip passed in as a parameter.
-		efxSource.clip = clip;
+		if (!soundPaused) 
+		{
+			//Set the clip of our efxSource audio source to the clip passed in as a parameter.
+			efxSource.clip = clip;
 
-		//Play the clip.
-		efxSource.Play();
+			//Play the clip.
+			efxSource.Play ();
+		}
 	}
 		
 }
