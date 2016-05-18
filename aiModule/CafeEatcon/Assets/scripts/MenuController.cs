e118ba7cead7f64394b018841200484466412727
hannahp <hpruse@128-223-223-104-wireless.uoregon.edu>
2016-05-18 14:31:38 -0700

    Issue #34 - Restart Button added to menu
    The buttons are showing up as different size across the different
    scenes. I'd imagine we'd want to change the icons later anyway, so I
    didn't bother changing them.

--- aiModule/CafeEatcon/Assets/scripts/MenuController.cs
+++ aiModule/CafeEatcon/Assets/scripts/MenuController.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
-	public static MenuController instance = null;
-
 
 	public void ToggleAudio()
 	{
 		SoundManager.instance.soundPaused = !SoundManager.instance.soundPaused;
 		AudioListener.pause = SoundManager.instance.soundPaused;
+	}
+
+	public void Restart()
+	{
+		Destroy (GlobalControl.Instance);
+		SceneManager.LoadScene ("phase1");
 	}
 }
