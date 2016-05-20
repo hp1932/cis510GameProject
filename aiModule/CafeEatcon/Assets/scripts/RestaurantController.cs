fafdb4ed72973858639d9f243b163f3224cb3a14
chapman3 <chapman3@uoregon.edu>
2016-05-19 21:21:31 -0700

    customer generating now dependent on serializables numcustomers

--- aiModule/CafeEatcon/Assets/scripts/RestaurantController.cs
+++ aiModule/CafeEatcon/Assets/scripts/RestaurantController.cs
@@ -60,7 +60,7 @@ public class RestaurantController : MonoBehaviour {
 	{
 		string[] ingredientList = localPlayerData.recipes [food];
 
-		localPlayerData.numCustomers++;
+		//localPlayerData.numCustomers++;
 
 		if (HaveAllIngredients (food)) {
 			if (food == "Ham Sandwich") {
