c1cd5acbdc6a4e8b9aea97fa9f41c74279e951f0
hannahp <hpruse@128-223-223-105-wireless.uoregon.edu>
2016-05-25 16:39:58 -0700

    Issue #49 - Added confirmation window for quit
    I am using a builtin modal window. You can override the style of it
    using GUIStyle.

--- aiModule/CafeEatcon/Assets/scripts/MenuController.cs
+++ aiModule/CafeEatcon/Assets/scripts/MenuController.cs
@@ -4,6 +4,8 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
+	private bool quitClicked = false;
+
 	public void ToggleAudio()
 	{
 		SoundManager.instance.soundPaused = !SoundManager.instance.soundPaused;
@@ -12,7 +14,28 @@ public class MenuController : MonoBehaviour {
 
 	public void Restart()
 	{
-		Destroy (GlobalControl.Instance);
-		SceneManager.LoadScene ("start");
+		quitClicked = true;
+		//Destroy (GlobalControl.Instance);
+		//SceneManager.LoadScene ("start");
+	}
+
+	public void OnGUI()
+	{
+		if (quitClicked) {
+			Rect windowRect = new Rect(75, 300, 248, 50);
+			windowRect = GUI.Window(0, windowRect, OpenConfirmation, "Are you sure you want to quit?");
+		}
+	}
+	void OpenConfirmation(int windowID) 
+	{
+		//GUI.Label (new Rect (0, 0, 248, 100), "Are you sure you want to quit?");
+		if (GUI.Button (new Rect (10, 20, 100, 20), "Yes")) {
+			Destroy (GlobalControl.Instance);
+			SceneManager.LoadScene ("start");
+			quitClicked = false;
+		}
+		if (GUI.Button (new Rect (130, 20, 100, 20), "No")) {
+			quitClicked = false;
+		}
 	}
 }
