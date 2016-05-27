acca01a99801d4bba3273c2d67d6d54ba29f0a65
Adam Bilbaeno <bilbaeno@uoregon.edu>
2016-05-26 23:26:34 -0700

    updated demand screen UI

--- aiModule/CafeEatcon/Assets/scripts/PricingGUI.cs
+++ aiModule/CafeEatcon/Assets/scripts/PricingGUI.cs
@@ -49,9 +49,9 @@ public class PricingGUI : MonoBehaviour {
 		GUI.Label(new Rect(xofGUI + 130,yofGUI - 25,30,20), avgMealCost.ToString());
 		//GUI.Label(new Rect(23, 435 - (PerCustCostY * guiLabelMultiplier), 35, 20), "$" + avgMealCost.ToString()); // Not sure why PerCustCostY is not working on its own here...
 		//GUI.Label (new Rect (200 + (PerCustCostX * guiLabelMultiplier), 373, 40, 20), potentialCustomers.ToString ()); // Not sure why PerCustCostX is not working on its own here either...
-		printHamSandwich.text = "Price of Ham Sandwich: $ " + hamSandwichCost.ToString("#.00");
-		printTurkeySandwich.text = "Price of Turkey Sandwich: $ " + turkeySandwichCost.ToString("#.00");
-		printVeggieSandwich.text = "Price of Turkey Sandwich: $ " + veggieSandwichCost.ToString("#.00");
+		printHamSandwich.text = "Ham Sandwich      $" + hamSandwichCost.ToString("#.00");
+		printTurkeySandwich.text = "Turkey Sandwich    $" + turkeySandwichCost.ToString("#.00");
+		printVeggieSandwich.text = "Veggie Sandwich    $" + veggieSandwichCost.ToString("#.00");
 
 	}
 
