87e39c42ad6ceb6f3d9fa9524f8a29cbe6f4ef6d
chapman3 <chapman3@uoregon.edu>
2016-06-05 19:23:38 -0700

    economy stories rate fixed

--- aiModule/CafeEatcon/Assets/scripts/NewspaperController.cs
+++ aiModule/CafeEatcon/Assets/scripts/NewspaperController.cs
@@ -28,7 +28,7 @@ public class NewspaperController : MonoBehaviour {
 		economyStories = new Dictionary<int, string> ();
 		normalStories = new Dictionary<int, string> ();
 		initializeStories();
-		econStoryRate = 1f;
+		econStoryRate = 0.5f;
 
 		dayText.text = "Day " + localPlayerData.currentDay;
 
